b22d9e54398640456af274217c6b8842c4c98ec2
derekearl <ear17004@byui.edu>
2024-10-18 20:19:06 -0600

    update09

---
+++ functions01/prep.py
@@ -0,0 +1,28 @@
+# This is just a review of past things using boolean, variables, ints, floats, etc
+
+a = "My name is " # string
+b = "Derek" # string
+c = a + b # string and a string makes a string
+print (f"a: {type(a)} {a}")
+print (f"a: {type(b)} {b}")
+print (f"a: {type(c)} {c}")
+print ()
+
+d = False # boolean
+e = True # boolean
+print (f'd: {type(d)} {d}')
+print (f'e: {type(e)} {e}')
+print ()
+
+f = 15 # int
+g = 7.54 # float
+h = f + g # int plus float equals float
+print (f'f: {type(f)} {f}')
+print (f'g: {type(h)} {h}')
+print (f'h: {type(h)} {h}')
+print ()
+
+i = "True" # This is a string becasue of the quotes
+j = "4.52" # Even though this is a number it is also a string because of quotes
+print (f'i: {type(i)} {i}')
+print (f'i: {type(i)} {i}')
