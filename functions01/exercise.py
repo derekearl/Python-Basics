50321a00c52efff4d1facd88d475552a875aa968
derekearl <ear17004@byui.edu>
2024-10-22 19:10:29 -0600

    update10

--- functions01/exercise.py
+++ functions01/exercise.py
@@ -2,7 +2,7 @@
 # Exercise 1 week 1 BYU- Idaho CSE 111
 
 # Get the input from the user (Age)
-answer = input ('What is your age? ')
+answer = input ('What is your current age? ')
 
 # Convert string into an interger
 age = int(answer)
@@ -10,11 +10,10 @@ age = int(answer)
 # Calculate slowest and fastest possible heart rate
 # Subtract age from 220 and multiply .65 for lowest
 # and .85 for the highest
-max_rate = 220 - age
-slowest_rate = max_rate * .65
-fastest_rate = max_rate * .85
+highest = (220 - age) * .85
+lowest = (220 - age) * .65 
 
 # Display the results
 print ()
-print (f"Your highest heart rate when exercising should be {fastest_rate:.0f} beats per minute \n"
-       f"and your lowest rate should be {slowest_rate:.0f} beats per minute ")
+print (f'Your highest heart rate should be: {highest:.0f}\n'
+       f'Your lowest heart rate should be: {lowest:.0f}')
--- functions01/prep.py
+++ functions01/prep.py
@@ -26,3 +26,46 @@ i = "True" # This is a string becasue of the quotes
 j = "4.52" # Even though this is a number it is also a string because of quotes
 print (f'i: {type(i)} {i}')
 print (f'i: {type(i)} {i}')
+
+# Further Examples
+# The input function will always return a string
+
+k = input ('Please enter a number: ')
+l = input ('Please enter another number: ')
+m = k + l  # String plus a string equals a string
+
+print (f'k: {type(k)} {k}')
+print (f'l: {type(l)} {l}')
+print (f'm: {type(m)} {m}') # This will display the first and second number combined
+
+# Convert the functions to int and float
+
+n = int(input('Please enter a whole number: '))
+o = float(input('Please enter a decimal number: '))
+p = n + o
+
+print(f"n: {type(n)} {n}")
+print(f"o: {type(o)} {o}")
+print(f"p: {type(p)} {p}")
+
+# Another Example
+
+# Given the distance that a cable will span and the distance
+# it will sag or dip in the middle, this program computes the
+# length of the cable.
+
+# Get user input and convert it from
+# strings to floating point numbers.
+span = float(input("Distance the cable must span in meters: "))
+dip = float(input("Distance the cable will sag in meters: "))
+
+# Use the numbers to compute the cable length.
+length = span + (8 * dip**2) / (3 * span)
+
+# Print the cable length in the
+# console window for the user to see.
+print(f"Length of cable in meters: {length:.2f}")
+
+
+
+
